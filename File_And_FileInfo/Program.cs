7ea11d70fb8aa068efdea6f540c04313785b5d29
AnaCarlaG <acarlagomes250@gmail.com>
2022-03-24 20:50:00 -0300

    criar e escrever no arquivo

---
+++ File_And_FileInfo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+
+WriteLine("Digite o nome do arquivo");
+var name = ReadLine();
+
+
+
+var path = Path.Combine(Environment.CurrentDirectory, $"{name}.txt");
+Name_Clean(name)
+Create_File(path);
+
+WriteLine("To end tap some key ...");
+ReadKey();
+
+static void Name_Clean(string name){
+foreach (var @char in Path.GetInvalidFileNameChars()) 
+{
+    name.Replace(@char, '_');
+}
+}
+//escrevendo no arquivo
+
+static void Create_File(string path)
+{
+    try
+    {
+        // Criar um arquivo
+        var write = File.CreateText(path);
+
+        write.WriteLine("I'm writing some text");
+        write.Flush();
+    }
+    catch
+    {
+        WriteLine("Name of file is invalid");
+    }
+
+}
+
+
